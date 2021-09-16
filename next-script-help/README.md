051c960fcfb31c2a134fa38c4a6d3120d057a748
nickskies <nicholas.saravia@worldacceptance.com>
2021-09-16 10:00:39 -0400

    initial

---
+++ next-script-help/README.md
@@ -0,0 +1,14 @@
+# Issue Documentation 00045007
+
+Outcome desired
+- when loading pages/fire.js
+-   1. jquery should be loaded 
+-   2. the dom should read "cats"
+
+Steps to reproduce issue.
+- pages/index.js has next/link to load pages/fire.js
+- error pops up that $ is not defined
+
+Questions
+- how do i acheive this with next/script
+- It seems no matter what strategy i use, the outcome desired does not work.
---
+++ next-script-help/pages/fire.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Script from 'next/script'
+import Head from 'next/head';
+
+const fire = () => {
+
+    return (
+        <div>
+            <Head>
+                <script dangerouslySetInnerHTML={{__html: "$('#cat').html('cats')"}}>
+                </script>
+            </Head>
+
+            {/* <Script id="two">{`$('#cat').html('cats')`}</Script> */}
+            <div id="cat"></div>            
+            <Script id="one" strategy="beforeInteractive" src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossOrigin="anonymous"></Script>
+
+        </div>
+    )
+}
+
+export default fire
